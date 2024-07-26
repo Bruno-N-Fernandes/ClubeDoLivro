4659f13f6fd0f0c8d6271336b1e2bcf14307c926
Bruno N Fernandes <bruno.tere@gmail.com>
2024-07-25 21:59:47 -0300

    Commit inicial do projeto em que foram desenvolvidas as classes de autor, emprestimo, livro e usuario

---
+++ Autor.cs
@@ -0,0 +1,9 @@
+﻿namespace ClubeDoLivro
+{
+	public class Autor
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Sobrenome { get; set; }
+	}
+}
---
+++ Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace ClubeDoLivro
+{
+	public class Usuario
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Email { get; set; }
+		public string Telefone { get; set; }
+		public string Senha { get; set; }
+	}
+}
