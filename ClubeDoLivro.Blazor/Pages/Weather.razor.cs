023611536b026d92f19438f2151756fcf1c1efc0
Bruno N Fernandes <bruno.tere@gmail.com>
2024-08-22 21:51:38 -0300

    Implementação inicial da tela do sistema com Blazor

---
+++ ClubeDoLivro.Blazor/Pages/Weather.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ClubeDoLivro.Blazor.Pages
+{
+    public partial class Weather
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await HttpClient.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+
+        public class WeatherForecast
+        {
+            public DateOnly Date { get; set; }
+
+            public int TemperatureC { get; set; }
+
+            public string? Summary { get; set; }
+
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
---
+++ ClubeDoLivro.Blazor/Program.cs
@@ -0,0 +1,23 @@
+
+global using System.Net.Http;
+global using System.Net.Http.Json;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace ClubeDoLivro.Blazor
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+			builder.RootComponents.Add<HeadOutlet>("head::after");
+
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			await builder.Build().RunAsync();
+		}
+	}
+}
