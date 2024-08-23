c7a29a04a8c97ba578c118f65f1ceb1942ca7ca1
Bruno N Fernandes <bruno.tere@gmail.com>
2024-08-22 21:03:57 -0300

    Inclusão do projeto de testes

---
+++ ClubeDoLivro.Testes/AutorTest.cs
@@ -0,0 +1,108 @@
+﻿using ClubeDoLivro.Domains;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDoLivro.Testes
+{
+	public class AutorTest
+	{
+		private readonly Autor _autor;
+		public AutorTest()
+		{
+			_autor = new Autor();
+		}
+
+		[Fact]
+		public void QuandoEuCrioUmAutor_OAutorDeveSerCriadoCorretamente()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Assert.NotNull(_autor);
+			Assert.Equal(0, _autor.Id);
+			Assert.NotNull(_autor.Livros);
+			Assert.Null(_autor.Nome);
+			Assert.Null(_autor.Sobrenome);
+		}
+
+		[Fact]
+		public void QuandoEuAdicionoUmLivroNoAutor_AQuantidadeDeLivrosEscritosDeveAumentarEm1Unidade()
+		{
+			//Arrange
+			var livro = TesteLivroFactory.ObterLivro();
+			var quantidadeDeLivrosAntes = _autor.LivrosEscritos;
+			var quantidadeEsperada = quantidadeDeLivrosAntes + 1;
+
+			//Act
+			_autor.AdicionarLivro(livro);
+
+			//Assert
+			Assert.Equal(quantidadeEsperada, _autor.LivrosEscritos);
+		}
+
+		[Fact]
+		public void QuandoEuAdicionoUmLivroNoAutor_OLivroPrecisaConterOAutorNaListaDeAutores()
+		{
+			//Arrange
+			var livro = TesteLivroFactory.ObterLivro();
+			var quantidadeDeLivrosAntes = _autor.LivrosEscritos;
+			var quantidadeEsperada = quantidadeDeLivrosAntes + 1;
+
+			//Act
+			_autor.AdicionarLivro(livro);
+
+			//Assert
+			Assert.True(livro.QuantidadeAutores == 1);
+			livro.FoiEscritoPelo(_autor).Should().BeTrue();
+		}
+
+		[Fact]
+		public void QuandoEuAdicionoDoisLivrosNoAutor_AutorPrecisaTerDoisLivros()
+		{
+			//Arrange
+			var livro1 = TesteLivroFactory.ObterLivro(1);
+			var livro2 = TesteLivroFactory.ObterLivro(2);
+			var quantidadeDeLivrosAntes = _autor.LivrosEscritos;
+			var quantidadeEsperada = quantidadeDeLivrosAntes + 2;
+
+			//Act
+			_autor.AdicionarLivro(livro1);
+			_autor.AdicionarLivro(livro2);
+
+			//Assert
+			Assert.Contains(livro1, _autor.Livros);
+			Assert.Contains(livro2, _autor.Livros);
+			_autor.LivrosEscritos.Should().Be(quantidadeEsperada);
+		}
+
+		[Fact]
+		public void QuandoEuDigoQueUmAutorEscreveuUmLivro_EsseLivroDeveConterOAutorEmSuaListaDeAutores()
+		{
+			//Arrange
+			var livro1 = TesteLivroFactory.ObterLivro();
+
+			//Act
+			_autor.AdicionarLivro(livro1);
+
+			//Assert
+			_autor.Livros.Should().NotBeEmpty();
+			_autor.Livros.Should().HaveCount(1);
+
+			livro1.QuantidadeAutores.Should().Be(1);
+		}
+	}
+
+	public class TesteLivroFactory
+	{
+		public static Livro ObterLivro(int id = 0)
+		{
+			return new Livro { Id = id, CodigoISBN = "113121", Edicao = "1", NomeDoLivro = "C#", Paginas = 1, Volume = "1" };
+		}
+	}
+}
---
+++ ClubeDoLivro.Testes/LivroTest.cs
@@ -0,0 +1,122 @@
+using ClubeDoLivro.Domains;
+using FluentAssertions;
+
+namespace ClubeDoLivro.Testes
+{
+	public class LivroTest
+	{
+		private readonly Livro _livro;
+
+		public LivroTest()
+        {
+			_livro = new Livro();
+        }
+
+
+		[Fact]
+		public void QuandoEuCrioUmLivro_OLivroDeveSerCriadoCorretamente()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Assert.NotNull(_livro);
+		}
+
+		[Fact]
+		public void QuandoEuCrioUmLivro_AQuantidadeDeAutoresDeveSerZero()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			_livro.QuantidadeAutores.Should().Be(0);
+		}
+
+		[Fact]
+		public void QuandoEuCrioUmLivro_APropriedadePaginasDeveSerNula()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			_livro.Paginas.Should().Be(0);
+		}
+
+		[Fact]
+		public void QuandoEuCrioUmLivro_APropriedadeEdicaoDeveSerNula()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Assert.Null(_livro.Edicao);
+		}
+
+		[Fact]
+		public void QuandoEuCrioUmLivro_APropriedadeNomeDoLivroDeveSerNula()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Assert.Null(_livro.NomeDoLivro);
+		}
+
+		[Fact]
+		public void QuandoEuCrioUmLivro_APropriedadeCodigoISBNDeveSerNula()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Assert.Null(_livro.CodigoISBN);
+		}
+
+		[Fact]
+		public void QuandoEuCrioUmLivro_APropriedadeVolumeDeveSerNula()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Assert.Null(_livro.Volume);
+		}
+
+		[Fact]
+		public void QuandoEuCrioUmLivro_APropriedadeIdDeveSerZero()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Assert.Equal(0, _livro.Id);
+		}
+
+
+		[Fact]
+		public void QuandoEuAdicionoUmAutorParaOLivro_EsseAutorPrecisaAtualizarAListaDeLivrosEscritos()
+		{
+			//Arrange
+			var autor = new Autor();
+			var livro = TesteLivroFactory.ObterLivro(1);
+
+			//Act
+			livro.AdicionarAutor(autor);
+
+			//Assert
+			livro.QuantidadeAutores.Should().Be(1);
+
+			autor.Livros.Should().NotBeEmpty();
+			autor.Livros.Should().HaveCount(1);
+		}
+	}
+}
