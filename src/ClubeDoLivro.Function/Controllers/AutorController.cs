935b3b8ede7d1f5d606517fda2c884c6ba589a82
Bruno N Fernandes <bruno.tere@gmail.com>
2024-09-18 21:41:50 -0300

    Implementação das APIs de CRUD de Autor

--- src/ClubeDoLivro.Function/Controllers/AutorController.cs
+++ src/ClubeDoLivro.Function/Controllers/AutorController.cs
@@ -1,3 +1,4 @@
+using ClubeDoLivro.Domains;
 using ClubeDoLivro.Function.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -5,6 +6,7 @@ using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -13,6 +15,13 @@ namespace ClubeDoLivro.Function.Controllers
 	public class AutorController : AbstractController
 	{
 		private const string ModelName = "Autor";
+
+		private readonly static Autor[] _autores = [
+			new Autor { Id = 1, Nome = "Bruno", Sobrenome = "Fernandes" },
+			new Autor { Id = 2, Nome = "Walmir", Sobrenome = "Oliveira" },
+			new Autor { Id = 3, Nome = "Ricardo", Sobrenome = "Castello"},
+			new Autor { Id = 4, Nome = "Glauber", Sobrenome = "Lucas"   },
+		];
 
 		public AutorController(IServiceProvider serviceProvider) : base(serviceProvider)
 		{
@@ -24,18 +33,92 @@ namespace ClubeDoLivro.Function.Controllers
 		[OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "application/json", typeof(Message), Description = "Unauthorized response")]
 		[OpenApiResponseWithBody(HttpStatusCode.BadRequest, "application/json", typeof(Message), Description = "BadRequest response")]
 		[OpenApiResponseWithBody(HttpStatusCode.NotFound, "application/json", typeof(Message), Description = "NotFound response")]
-		[OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(string), Description = "OK response")]
+		[OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(Autor), Description = "OK response")]
 		public async Task<dynamic> GetAll([HttpTrigger(AuthorizationLevel.Anonymous, "Get", Route = ModelName)] HttpRequestData httpRequestData)
 		{
 			try
 			{
-				var json = new[] {
-					new { Id = 1, Nome = "Bruno", Sobrenome = "Fernandes", Livros = new object[0] },
-					new { Id = 2, Nome = "Walmir", Sobrenome = "Oliveira", Livros = new object[0] },
-					new { Id = 3, Nome = "Ricardo", Sobrenome = "Castello", Livros = new object[0] },
-					new { Id = 4, Nome = "Glauber", Sobrenome = "Lucas", Livros = new object[0] },
-				};
-				return json;
+				return new OkObjectResult(_autores);
+			}
+			catch (Exception exception)
+			{
+				return await Task.FromResult(new BadRequestObjectResult(new Message(exception.Message)));
+			}
+		}
+
+		[Function(ModelName + "GetOne")]
+		[OpenApiOperation(ModelName + "GetOne", ModelName, Summary = "Listar todos os autores", Description = "Use para Listar todos os Autores")]
+		[OpenApiParameter("id", In = ParameterLocation.Path)]
+		[OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "application/json", typeof(Message), Description = "Unauthorized response")]
+		[OpenApiResponseWithBody(HttpStatusCode.BadRequest, "application/json", typeof(Message), Description = "BadRequest response")]
+		[OpenApiResponseWithBody(HttpStatusCode.NotFound, "application/json", typeof(Message), Description = "NotFound response")]
+		[OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(Autor), Description = "OK response")]
+		public async Task<dynamic> GetOne([HttpTrigger(AuthorizationLevel.Anonymous, "Get", Route = ModelName + "/{id}")] HttpRequestData httpRequestData, int id)
+		{
+			try
+			{
+				var autor = _autores.FirstOrDefault(x => x.Id == id);
+				return new OkObjectResult(autor);
+			}
+			catch (Exception exception)
+			{
+				return await Task.FromResult(new BadRequestObjectResult(new Message(exception.Message)));
+			}
+		}
+
+
+		[Function(ModelName + "Create")]
+		[OpenApiOperation(ModelName + "Create", ModelName, Summary = "Cria um novo autor", Description = "Use para criar um novo Autor")]
+		[OpenApiRequestBody("application/json", typeof(Autor), Description = "Json contendo os dados do Autor", Required = true)]
+		[OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "application/json", typeof(Message), Description = "Unauthorized response")]
+		[OpenApiResponseWithBody(HttpStatusCode.BadRequest, "application/json", typeof(Message), Description = "BadRequest response")]
+		[OpenApiResponseWithBody(HttpStatusCode.NotFound, "application/json", typeof(Message), Description = "NotFound response")]
+		[OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(Autor), Description = "OK response")]
+		public async Task<dynamic> Create([HttpTrigger(AuthorizationLevel.Anonymous, "Post", Route = ModelName)] HttpRequestData httpRequestData)
+		{
+			try
+			{
+				return new OkResult();
+			}
+			catch (Exception exception)
+			{
+				return await Task.FromResult(new BadRequestObjectResult(new Message(exception.Message)));
+			}
+		}
+
+		[Function(ModelName + "Update")]
+		[OpenApiOperation(ModelName + "Update", ModelName, Summary = "Atualiza o autor", Description = "Use para atualiza o Autor")]
+		[OpenApiRequestBody("application/json", typeof(Autor), Description = "Json contendo os dados do Autor", Required = true)]
+		[OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "application/json", typeof(Message), Description = "Unauthorized response")]
+		[OpenApiResponseWithBody(HttpStatusCode.BadRequest, "application/json", typeof(Message), Description = "BadRequest response")]
+		[OpenApiResponseWithBody(HttpStatusCode.NotFound, "application/json", typeof(Message), Description = "NotFound response")]
+		[OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(Autor), Description = "OK response")]
+		public async Task<dynamic> Update([HttpTrigger(AuthorizationLevel.Anonymous, "Put", Route = ModelName + "/{id}")] HttpRequestData httpRequestData, int id)
+		{
+			try
+			{
+				var autor = _autores.FirstOrDefault(x => x.Id == id);
+				// change...
+				return new OkObjectResult(autor);
+			}
+			catch (Exception exception)
+			{
+				return await Task.FromResult(new BadRequestObjectResult(new Message(exception.Message)));
+			}
+		}
+
+		[Function(ModelName + "Delete")]
+		[OpenApiOperation(ModelName + "Delete", ModelName, Summary = "Apaga um autor", Description = "Use para apagar o Autor")]
+		[OpenApiRequestBody("application/json", typeof(Autor), Description = "Json contendo os dados do Autor", Required = true)]
+		[OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "application/json", typeof(Message), Description = "Unauthorized response")]
+		[OpenApiResponseWithBody(HttpStatusCode.BadRequest, "application/json", typeof(Message), Description = "BadRequest response")]
+		[OpenApiResponseWithBody(HttpStatusCode.NotFound, "application/json", typeof(Message), Description = "NotFound response")]
+		[OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(Autor), Description = "OK response")]
+		public async Task<dynamic> Delete([HttpTrigger(AuthorizationLevel.Anonymous, "Delete", Route = ModelName + "/{id}")] HttpRequestData httpRequestData, int id)
+		{
+			try
+			{
+				return new OkResult();
 			}
 			catch (Exception exception)
 			{
